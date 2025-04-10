ffd49c12d2025a02b2373633174d23950496c6e4
U1T-HoQuocViet <24521995@gm.uit.edu.vn>
2025-04-10 09:55:16 +0700

    12345

---
+++ eww/sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+//void SelectionSort(vector<int> & a)
+//{
+//   int size = a.size();
+//   for(int i = 0; i < size; i++)
+//   {
+//       int sIndex = i;
+//       for(int j = i; j < size; j++)
+//       {
+//           if(a[j] < a[sIndex])
+//               sIndex = j;
+//       }
+//       swap(a[i] ,a[sIndex]);
+//   }
+//}
+//
+//void InsertionSort(vector<int> & a)
+//{
+//   int size = a.size();
+//   for(int i = 1; i < size; i++)
+//   {
+//       int _s = a[i];
+//       int j = i-1;
+//       while(j >= 0 && a[j] > a[_s])
+//       {
+//           a[j+1] = a[j];
+//           j -= 1;
+//       }
+//       a[j+1] = _s;
+//   }
+//}
+
